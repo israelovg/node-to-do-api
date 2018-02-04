dc69750a44e1c737ffdd2bbec9828172fb157de2
Gaby Israeli <israelovg@outlook.com>
2018-02-04 21:28:53 +0200

    add file to repository

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,32 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+//es6 object destructuting - we can init object properties this way - var {name}={name:'Gabi',age:34};
+const { MongoClient,ObjectID } = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/todoApp',(error, db) => {
+    if (error) {
+        return console.log('unable to connect mongodb Server.');
+    }
+    console.log("Succesfully connected to mongodb Server");
+
+    //db.collection('todo').insertOne(
+    //    { text: 'some thing to do more' ,completed:true},
+    //    (err, result) => {
+    //        if (err) {
+    //            return console.log('unable to insert todo', err);
+    //        }
+    //        console.log(JSON.stringify(result.ops,undefined,2));
+    //    }
+    //);
+    db.collection('users').insertOne(
+        { name: 'Gabi', age: 38,location:"Holon ,Israel" },
+        (err, result) => {
+            if (err) {
+                return console.log('unable to insert user', err);
+            }
+            //here we print object that inserted to mongo
+            console.log(JSON.stringify(result.ops, undefined, 2));
+            //id property that generated contains , timespam
+            console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+        }
+    );
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+//es6 object destructuting - we can init object properties this way - var {name}={name:'Gabi',age:34};
+const { MongoClient,ObjectID } = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/todoApp',(error, db) => {
+    if (error) {
+        return console.log('unable to connect mongodb Server.');
+    }
+    console.log("Succesfully connected to mongodb Server");
+
+    //db.collection('todo').find({
+    //    _id:new ObjectID("5a6c9ef3c0b4c334381d76c1") // we can query results by 
+    //}).toArray().then((docs) => {
+    //    console.log("todo");
+    //    console.log(JSON.stringify(docs,undefined,2));
+    //} ,(err) => {
+    //    console.log("can't fetch todo", error);
+    //});
+
+    db.collection('users').find({name:"Gabi"}).count().then((count) => {
+        console.log(`users count:${count}`);    
+    } ,(err) => {
+        console.log("can't fetch users", error);
+    });
+    // db.close();
+});
