2af2463b08d37e88331db3a7ce1ca87c0b3bee6a
Gaby Israeli <israelovg@outlook.com>
2018-03-12 19:39:04 +0200

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@
 var bodyParser = require('body-parser');
 
 var mongoose = require("./db/mongoose");
-var {Todo} = require('./models/todo');
-var {User}  = require('./models/user');}
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+const { ObjectId } = require('mongodb');
 
 var app = express();
 
@@ -14,8 +15,8 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
     todo.save().then((doc) => {
-            res.send(doc);
-        },
+        res.send(doc);
+    },
         (error) => {
             res.status(400).send(error);
         });
@@ -23,12 +24,31 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-                res.send({ todos });
-               },
-               (error) => res.status(400).send(error));
+        res.send({ todos });
+    },
+        (error) => res.status(400).send(error));
 });
+
+// GET/todos/id
+app.get('/todos/:id', (req, res) => {
+    if (! ObjectId.isValid(req.params.id)) {
+       return   res.status(404).send();
+    }
+
+    Todo.findById(req.params.id).then((todo) => {
+        if (!todo) {
+          return  res.status(404).send();
+        }
+        
+        res.send(todo);
+    },
+        (error) => {
+            res.status(400).send(error);
+        });
+});
+
 app.listen(3000,
     () => {
-        console.log("Started on port 3000");
+        console.log("Started on port 3000 node-todo-api/server");
     });
-module.exports = app ;
+module.exports = app;
